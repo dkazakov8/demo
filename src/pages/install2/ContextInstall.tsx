96cf2cd5092514d1e1db731cd7b9288b73fa3fa7
Dmitry Kazakov <dmitry.kazakov.developer@gmail.com>
2024-12-06 20:10:23 +0300

    remove install 2

--- src/pages/install2/ContextInstall.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-import { StoreInstall } from './store';
-
-export const ContextInstall = createContext<StoreInstall | null>(null);
--- src/pages/install2/store.ts
+++
@@ -1,9 +0,0 @@
-import { makeAutoObservable } from 'mobx';
-
-export class StoreInstall {
-  constructor() {
-    makeAutoObservable(this, {}, { autoBind: true });
-  }
-
-  data = 1;
-}
